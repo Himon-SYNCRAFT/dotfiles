73051a6c7a4e8f94ae9f39f08c8945a183d3f03f
Daniel Zawłocki <d.zawlocki@syncraft.pl>
2020-11-22 09:26:30 +0100

    settings

--- fish/.config/fish/functions/oc.fish
+++ fish/.config/fish/functions/oc.fish
@@ -2,7 +2,11 @@ function oc
     set file (find $HOME/.config/ -type f -follow -print | fzf --height 10 -e | sed 's/ *$//')
 
     if test -n "$file"
-        vim "$file"
+        if ! command -v nvim &> /dev/null
+            vim "$file"
+        else
+            nvim "$file"
+        end
     end
     # find $HOME/.config/ -type f -follow -print | fzf --height 10 -e | xargs -o vim
 end
