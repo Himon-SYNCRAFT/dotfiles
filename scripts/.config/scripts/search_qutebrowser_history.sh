001288c6b3342b6806420214f3dd690a85538fe0
Daniel Zawlocki <d.zawlocki@syncraft.pl>
2021-07-27 11:14:27 +0200

    commit

--- scripts/.config/scripts/search_qutebrowser_history.sh
+++ scripts/.config/scripts/search_qutebrowser_history.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-url=$(sqlite3 ~/.local/share/qutebrowser/history.sqlite "select url from history" | sort | uniq | sed '/^chrome/d' | sed '/^about/d'| sed '/^qute/d' | sed '/^http:\/\/localhost/d' | dmenu -l 10 -p "browser history:")
+url=$(sqlite3 ~/.local/share/qutebrowser/history.sqlite "select url from history" | sort | uniq | sed '/^chrome/d' | sed '/^about/d'| sed '/^qute/d' | sed '/^http:\/\/localhost/d' | sed '/duckduckgo/d' | dmenu -l 20 -p "browser history:")
 
 if [ -n "$url" ]; then
     qutebrowser "$url"
