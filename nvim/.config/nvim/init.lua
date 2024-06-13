efc823b38eb4e0d5781a6bfeae948a42cdf97197
Daniel Zawlocki <d.zawlocki@syncraft.pl>
2024-06-13 21:45:01 +0200

    lazy dev - tez nie widze, zeby dzialalo

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -173,6 +173,13 @@ require("lazy").setup({
 
 	{
 		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			})
+		end,
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
@@ -213,6 +220,19 @@ require("lazy").setup({
 	},
 
 	{ "tjdevries/templ.nvim" },
+
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{ "Bilal2453/luvit-meta", lazy = true },
 })
 
 require("user.dressing")
