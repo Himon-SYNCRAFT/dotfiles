c6472774e6d3090c48aa7280c3167f9b1a83946c
Daniel Zawlocki <d.zawlocki@syncraft.pl>
2024-09-23 14:35:22 +0200

    commit

--- fish/.config/fish/functions/db.fish
+++ fish/.config/fish/functions/db.fish
@@ -1,3 +1,3 @@
 function db --wraps='nvim +DBUI' --description 'alias db nvim +DBUI'
-  nvim "+let g:auto_session_enabled = v:false" +DBUI $argv
+  nvim --cmd "let g:auto_session_enabled = v:false" +DBUI $argv
 end
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -104,39 +104,39 @@ require("lazy").setup({
 			})
 		end,
 	},
-	{
-		"https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
-		branch = "master",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		event = "FileType",
-		enabled = true,
-		config = function()
-			local rainbow_delimiters = require("rainbow-delimiters")
-			require("rainbow-delimiters.setup").setup({
-				strategy = {
-					[""] = rainbow_delimiters.strategy["global"],
-					vim = rainbow_delimiters.strategy["local"],
-				},
-				query = {
-					[""] = "rainbow-delimiters",
-					lua = "rainbow-blocks",
-				},
-				priority = {
-					[""] = 110,
-					lua = 210,
-				},
-				highlight = {
-					"RainbowDelimiterRed",
-					"RainbowDelimiterYellow",
-					"RainbowDelimiterBlue",
-					"RainbowDelimiterOrange",
-					"RainbowDelimiterGreen",
-					"RainbowDelimiterViolet",
-					"RainbowDelimiterCyan",
-				},
-			})
-		end,
-	},
+	-- {
+	-- 	"https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
+	-- 	branch = "master",
+	-- 	dependencies = "nvim-treesitter/nvim-treesitter",
+	-- 	event = "FileType",
+	-- 	enabled = true,
+	-- 	config = function()
+	-- 		local rainbow_delimiters = require("rainbow-delimiters")
+	-- 		require("rainbow-delimiters.setup").setup({
+	-- 			strategy = {
+	-- 				[""] = rainbow_delimiters.strategy["global"],
+	-- 				vim = rainbow_delimiters.strategy["local"],
+	-- 			},
+	-- 			query = {
+	-- 				[""] = "rainbow-delimiters",
+	-- 				lua = "rainbow-blocks",
+	-- 			},
+	-- 			priority = {
+	-- 				[""] = 110,
+	-- 				lua = 210,
+	-- 			},
+	-- 			highlight = {
+	-- 				"RainbowDelimiterRed",
+	-- 				"RainbowDelimiterYellow",
+	-- 				"RainbowDelimiterBlue",
+	-- 				"RainbowDelimiterOrange",
+	-- 				"RainbowDelimiterGreen",
+	-- 				"RainbowDelimiterViolet",
+	-- 				"RainbowDelimiterCyan",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 
 	-- Telescope Extensions
 	{ "nvim-telescope/telescope.nvim" },
