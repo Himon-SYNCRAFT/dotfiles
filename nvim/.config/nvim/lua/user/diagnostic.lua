1177f392ccdd68293ffbb212786d36ad96e1929a
Daniel Zawlocki <d.zawlocki@syncraft.pl>
2025-03-06 07:42:00 +0100

    commit

--- nvim/.config/nvim/lua/user/diagnostic.lua
+++ nvim/.config/nvim/lua/user/diagnostic.lua
@@ -1,11 +1,11 @@
 vim.diagnostic.config({
 	signs = {
-		text = {
-			[vim.diagnostic.severity.ERROR] = "",
-			[vim.diagnostic.severity.WARN] = "",
-			[vim.diagnostic.severity.INFO] = "",
-			[vim.diagnostic.severity.HINT] = "",
-		},
+		-- text = {
+		-- 	[vim.diagnostic.severity.ERROR] = "",
+		-- 	[vim.diagnostic.severity.WARN] = "",
+		-- 	[vim.diagnostic.severity.INFO] = "",
+		-- 	[vim.diagnostic.severity.HINT] = "",
+		-- },
 		numhl = {
 			[vim.diagnostic.severity.WARN] = "WarningMsg",
 			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
