35d6901ec90e10c89624c95b4c4f98f480ca0eeb
Daniel Zawlocki <d.zawlocki@syncraft.pl>
2026-07-09 21:48:14 +0200

    commit

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -9,7 +9,7 @@
 -- Create your files separately and then require them like this:
 -- require("myColors")
 
-require("hyprland-gui")
+-- require("hyprland-gui")
 
 ------------------
 ---- MONITORS ----
