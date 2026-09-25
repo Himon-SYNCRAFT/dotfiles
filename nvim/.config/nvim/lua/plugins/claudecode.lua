3cbca3e4dad4928e7f89eba67f37cf75ee5ecb5d
Daniel Zawłocki <d.zawlocki@eltrox.pl>
2026-09-25 06:08:54 +0000

    commit

--- nvim/.config/nvim/lua/plugins/claudecode.lua
+++ nvim/.config/nvim/lua/plugins/claudecode.lua
@@ -4,12 +4,12 @@ require("claudecode").setup({
 		split_width_percentage = 0.50,
 		provider = "external",
 		provider_opts = {
-			-- external_terminal_cmd = "footclient --working-directory %s -e %s",
+			external_terminal_cmd = "foot --working-directory %s -e %s",
 			-- external_terminal_cmd = "alacritty msg create-window --working-directory %s -e %s",
-			external_terminal_cmd = "alacritty --working-directory %s -e %s",
+			-- external_terminal_cmd = "alacritty --working-directory %s -e %s",
 		},
 	},
-	terminal_cmd = "claude --dangerously-skip-permissions",
+	terminal_cmd = "/home/himon/.local/bin/claude --dangerously-skip-permissions",
 })
 
 vim.keymap.set("n", "<leader>ac", "<cmd>ClaudeCode<cr>", { desc = "Toggle Claude" })
